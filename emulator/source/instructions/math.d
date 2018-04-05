962a3f806a83babf2ddb0879a434b2774d6bccef
Jacob Reckhard <jacobreckhard@gmail.com>
2018-04-04 21:05:39 -0600

    added more instructions
    ran into a problem with endieness

--- emulator/source/instructions/math.d
+++ emulator/source/instructions/math.d
@@ -6,5 +6,49 @@ import instructions.instructions;
 
 
 void add(ref Cpu chip){
-    
+    assert(matches(chip.opcode, "1101 ... . 10 ......"));
+
+    ubyte data =cast(ubyte) getBits(chip.opcode, 4, 3);
+    ubyte other = cast(ubyte)getBits(chip.opcode, 10, 6);
+    ubyte incoming =cast(ubyte) getBits(chip.opcode, 7, 1);
+
+    int* src;
+    int* dst;
+    if(incoming == 1){
+        src = getMem(chip, 0b000, data, SIZE_LONG);
+        dst = getDest(chip, other, SIZE_LONG);
+    }else{
+        src = getSource(chip, other, SIZE_LONG);
+        dst = getMem(chip, 0b000, data, SIZE_LONG);
+    }
+
+    int val = readLoc(dst, SIZE_LONG) + readLoc(src, SIZE_LONG);
+    writeLoc(dst, SIZE_LONG, &val);
+
+
+}
+
+unittest{
+    Cpu chip;
+    for(int i = 0; i < 1024*64; i++){
+        chip.ram[i] = cast(ubyte) i;
+    }
+    for(int i = 0; i < 8; i++){
+        chip.A[i] = i+1;
+        chip.D[i] = i+1;
+    }
+
+    chip.pc = 0;
+    ushort instr = 0b1101_001_110_000000; // Add d1 to d0 and store in d0
+    chip.opcode = instr;
+    add(chip);
+    assert_eq(3, chip.D[0]);
+    assert_eq(2, chip.D[1]);
+
+    instr = 0b1101_001_010_000000; // Add d1 to d0 and store in d1
+    chip.opcode = instr;
+    add(chip);
+    assert_eq(3, chip.D[0]);
+    assert_eq(5, chip.D[1]);
+
 }
--- emulator/source/instructions/other.d
+++ emulator/source/instructions/other.d
@@ -5,5 +5,56 @@ import instructions.instructions;
 
 
 void trap(ref Cpu chip){
-    
+    immutable trap_num = getBits(chip.opcode, 12, 4);
+    switch(trap_num){
+        case 15:
+            trap15(chip);
+            break;
+        default:
+            import std.stdio;
+            writef("Error, trap #%d is not defined\n", trap_num);
+            assert(0);
+    }
+
+}
+
+void trap15(ref Cpu chip){
+    import std.stdio;
+    switch(chip.D[0]){
+        case 15:
+            writeln(get_number_string(chip.D[1], chip.D[2]));
+            break;
+        default:
+            writef("Error, trap #15 task %d is not defined\n", chip.D[0]);
+            assert(0);
+    }
+
+}
+
+immutable string basechars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+string get_number_string(int num, int base){
+    string ans = "";
+    while (num > 0){
+        ans = basechars[num % base] ~ ans;
+        num /= base;
+    }
+    return ans;
+}
+
+unittest{
+    Cpu chip;
+    for(int i = 0; i < 1024*64; i++){
+        chip.ram[i] = cast(ubyte) i;
+    }
+    for(int i = 0; i < 8; i++){
+        chip.A[i] = i;
+        chip.D[i] = i;
+    }
+
+    ushort instr = 0b010011100100_1111;
+
+    assert("A" == get_number_string(10, 16));
+    assert("Z" == get_number_string(35, 36));
+    assert("49" == get_number_string(49, 10));
+    assert("101" == get_number_string(5, 2));
 }
